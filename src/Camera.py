cb4c42c1b4d273ce6c549893d9c13a135faf4685
ayberkderingoz <84870113+ayberkderingoz@users.noreply.github.com>
2023-01-03 12:11:55 +0300

    opencv update

--- src/Camera.py
+++ src/Camera.py
@@ -1,4 +1,5 @@
 import cv2
+import torch
 import numpy as np
 import os
 from matplotlib import pyplot as plt
@@ -7,6 +8,7 @@ import mediapipe as mp
 
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
+model = torch.load("LOCATION")
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -39,7 +41,26 @@ def draw_styled_landmarks(image, results):
                              mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                              )
+def preprocess_frame(frame):
+    # Resize the frame to a specific size
+    frame = cv2.resize(frame, (224, 224))
 
+    # Convert the frame to a numpy array
+    frame = np.array(frame)
+
+    # Normalize the frame
+    frame = frame / 255.0
+
+    # Add an additional dimension to the frame (since the model expects a 4D tensor as input)
+    frame = np.expand_dims(frame, axis=0)
+
+    return frame
+def process_predictions(predictions, frame):
+    # Extract the predicted class from the predictions
+    predicted_class = np.argmax(predictions)
+
+    # Add the predicted class to the frame
+    cv2.putText(frame, str(predicted_class), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)    
 cap = cv2.VideoCapture(0)
 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
@@ -47,14 +68,26 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         ret, frame = cap.read()
 
+        input_data = preprocess_frame(frame)
 
-        image, results = mediapipe_detection(frame, holistic)
-        print(image)
+
+        input_tensor = torch.tensor(input_data).float()
+
+
+        predictions = model(input_tensor)
+    
+
+        process_predictions(predictions, frame)
+
+
+        cv2.imshow('Frame', frame)
+        #image, results = mediapipe_detection(frame, holistic)
+        #print(image)
         
 
-        draw_styled_landmarks(image, results)
+        #draw_styled_landmarks(image, results)
 
-        cv2.imshow('OpenCV Feed', image)
+        #cv2.imshow('OpenCV Feed', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
@@ -62,5 +95,5 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     cv2.destroyAllWindows()
 
 
-draw_landmarks(frame, results)
+#draw_landmarks(frame, results)
 plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
